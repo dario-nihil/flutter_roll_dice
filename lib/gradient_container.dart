ddbeee100cf63ea9c95b1d85a87753f16663f4b3
dario-nihil <dario.nihil@gmail.com>
2024-02-27 14:07:57 +0100

    customized StyledText widget to accept parameter text, added dice asstets images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 
-import 'package:roll_dice/styled_text.dart';
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  final List<Color> colors;
+
+  const GradientContainer(this.colors, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText(),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,16 @@ import 'package:flutter/material.dart';
 
 import 'package:roll_dice/gradient_container.dart';
 
+List<Color> colors = const [
+  Color.fromARGB(255, 26, 2, 80),
+  Color.fromARGB(255, 45, 7, 98)
+];
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(colors),
       ),
     ),
   );
